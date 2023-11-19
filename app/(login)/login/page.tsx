e0a95a63b712b218237d04c713d54ad9858bc4f9
kjw-blog <rkdwjddnr11@naver.com>
2023-11-19 23:50:08 +0900

    feat: 로그인 페이지
    - 커스텀 input 수정
    - 로그인 페이지 적용

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { z } from 'zod';
-import { signIn } from 'next-auth/react';
-import { Input } from '@/components/common/input';
+import Input from '@/components/common/input';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const LoginForm = z.object({
   userId: z.string().min(1, {
@@ -14,28 +14,34 @@ const LoginForm = z.object({
 });
 
 export default function LoginPage() {
-  const form = useForm();
+  const { register, handleSubmit } = useForm<z.infer<typeof LoginForm>>({
+    resolver: zodResolver(LoginForm),
+  });
 
-  const onLogin = () => {};
+  const onLogin = (data: z.infer<typeof LoginForm>) => {
+    console.log(data);
+  };
 
   return (
-    <form className="flex flex-1 flex-col space-y-12">
+    <form
+      onSubmit={handleSubmit(onLogin)}
+      className="flex flex-1 flex-col space-y-12"
+    >
       <Input
         label="아이디"
         placeholder="아이디를 입력해 주세요."
         type="text"
         warning="아이디를 입력해 주세요."
+        register={register('userId')}
       />
       <Input
         label="비밀번호"
         placeholder="비밀번호를 입력해 주세요."
         type="password"
         warning="비밀번호를 입력해 주세요."
+        register={register('userPw')}
       />
-      <button
-        className="w-full rounded-sm bg-camp-heavy py-2 text-center font-semibold text-white"
-        onClick={onLogin}
-      >
+      <button className="w-full rounded-sm bg-camp-heavy py-2 text-center font-semibold text-white">
         로그인
       </button>
     </form>
--- components/common/input.tsx
+++ components/common/input.tsx
@@ -1,11 +1,18 @@
+import React from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
 interface InputProps {
   label: string;
   type: 'text' | 'password';
   placeholder: string;
   warning?: string;
+  register?: UseFormRegisterReturn;
 }
 
-export const Input = ({ label, type, placeholder, warning }: InputProps) => {
+const Input = (
+  { label, type, placeholder, warning, register }: InputProps,
+  ref: React.Ref<HTMLInputElement>,
+) => {
   return (
     <div className="relative w-full rounded-sm border border-zinc-300">
       <input
@@ -13,6 +20,7 @@ export const Input = ({ label, type, placeholder, warning }: InputProps) => {
         required
         className="peer w-full rounded-sm px-[10px] py-4 text-sm text-zinc-500 outline-none focus:placeholder:opacity-0"
         placeholder={placeholder}
+        {...register}
       />
       <label className="absolute left-[10px] top-4 bg-white px-1 text-sm text-zinc-500 opacity-0 transition peer-valid:-translate-y-[26px] peer-valid:text-camp-heavy peer-valid:opacity-100 peer-focus:-translate-y-[26px] peer-focus:text-camp-heavy peer-focus:opacity-100">
         {label}
@@ -23,3 +31,5 @@ export const Input = ({ label, type, placeholder, warning }: InputProps) => {
     </div>
   );
 };
+
+export default React.forwardRef(Input);
