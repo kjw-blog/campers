a14a63b37a4dd11f51f1277226ae30f9a78ebc06
kjw-blog <rkdwjddnr11@naver.com>
2023-11-20 21:03:26 +0900

    chore: 설정
    - user table 수정
    - axios 설치
    - middleware path 추가

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { getToken } from 'next-auth/jwt';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   const token = await getToken({ req: request });
 
   if (!token) {
