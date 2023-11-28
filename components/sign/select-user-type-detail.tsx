cba7aca8b0919c29a4a11dce25a94b518f833618
kjw-blog <rkdwjddnr11@naver.com>
2023-11-28 23:24:04 +0900

    style: 스타일 수정
    - 유저 타입 선택창 css 수정

--- components/sign/select-user-type-detail.tsx
+++ components/sign/select-user-type-detail.tsx
@@ -25,12 +25,12 @@ export const SelectUserTypeDetail = ({ type }: SelectUserTypeDetailProps) => {
   return (
     <Link
       href={`/signup?type=${type}`}
-      className="group flex h-60 flex-1 cursor-pointer select-none flex-col overflow-hidden rounded-md border border-zinc-300 shadow-md transition hover:-translate-y-3"
+      className="group flex h-60 flex-1 cursor-pointer select-none flex-col overflow-hidden rounded-md border border-zinc-300 transition duration-500 hover:-translate-y-2 hover:shadow-xl"
     >
-      <div className="dark:bg-dark-200 flex flex-1 items-center justify-center border-b-[1px] transition group-hover:bg-camp-heavy dark:border-zinc-400">
+      <div className="flex flex-1 items-center justify-center border-b-[1px] transition group-hover:bg-camp-heavy dark:border-zinc-400 dark:bg-dark-200">
         <Icon className="h-16 w-16 stroke-camp-heavy transition group-hover:scale-125 group-hover:stroke-white" />
       </div>
-      <div className="dark:bg-dark-100 flex flex-col items-center justify-center space-y-2 py-2">
+      <div className="flex flex-col items-center justify-center space-y-2 py-2 dark:bg-dark-100">
         <p className="font-semibold text-camp-heavy">{kr}</p>
         <p className="text-xs text-zinc-700 dark:text-zinc-400">
           {description}
