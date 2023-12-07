7323f950d43f95fe3b873e668a74f415b3399154
kjw-blog <rkdwjddnr11@naver.com>
2023-12-07 19:48:52 +0900

    feat: 호스트 페이지
    - 모바일 컴포넌트 분리

---
+++ components/host/mobile-toggle.tsx
@@ -0,0 +1,23 @@
+import { Menu } from 'lucide-react';
+
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
+import { NavigationSidebar } from './navigation-sidebar';
+import { CampSidebar } from './camp-sidebar';
+
+export const MobileToggle = ({ campId }: { campId: string }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <button className="md:hidden">
+          <Menu />
+        </button>
+      </SheetTrigger>
+      <SheetContent side="left" className="flex gap-0 p-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <CampSidebar campId={campId} />
+      </SheetContent>
+    </Sheet>
+  );
+};
