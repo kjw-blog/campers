db08c3993f806569be7d3e2d92b05cb3daa2414e
kjw-blog <rkdwjddnr11@naver.com>
2023-12-08 19:56:34 +0900

    style: 스타일 수정
    - 모바일 환경에서 camp sidebar의 크기가 적절하지 않은 css 수정
    - 객실 수정, 삭제 버튼 hover color 추가

--- components/host/room-item.tsx
+++ components/host/room-item.tsx
@@ -14,14 +14,14 @@ export const RoomItem = ({ room, campId }: RoomItemProps) => {
   const { openModal } = useModalStore();
 
   return (
-    <div className="group flex h-12 w-full cursor-pointer select-none items-center space-x-2 overflow-hidden border-b-[1px] border-zinc-400 px-3 transition hover:bg-zinc-700/10 dark:border-zinc-700 dark:hover:bg-zinc-700/50 ">
+    <div className="group flex h-12 w-[322px] cursor-pointer  select-none items-center space-x-2 overflow-hidden border-b-[1px] border-zinc-400 px-3 transition hover:bg-zinc-700/10 dark:border-zinc-700 dark:hover:bg-zinc-700/50 md:w-60 ">
       <span className="block flex-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
         {room.name}
       </span>
       <div className="ml-auto hidden space-x-2 group-hover:flex">
         <TooltipWrapper label="객실명 수정" side="top">
           <Edit
-            className="h-4 w-4"
+            className="h-4 w-4 transition hover:stroke-camp-heavy"
             onClick={() =>
               openModal('room-manage', { campId, room, title: '객실명 수정' })
             }
@@ -29,7 +29,7 @@ export const RoomItem = ({ room, campId }: RoomItemProps) => {
         </TooltipWrapper>
         <TooltipWrapper label="객실 삭제" side="top">
           <Trash2
-            className="h-4 w-4"
+            className="h-4 w-4 transition hover:stroke-rose-500"
             onClick={() => openModal('room-delete', { campId, room })}
           />
         </TooltipWrapper>
