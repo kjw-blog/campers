a80070b7b29acaa35f3f98c22cbc69477f8eacda
kjw-blog <rkdwjddnr11@naver.com>
2023-12-15 17:12:12 +0900

    feat: 테스트 계정 표출

--- app/(login)/login/layout.tsx
+++ app/(login)/login/layout.tsx
@@ -8,14 +8,20 @@ export default function LoginLayout({
   return (
     <div className="relative flex w-[420px] max-w-[90%] rounded-sm border border-zinc-200 bg-white p-4 py-10 shadow-md dark:bg-dark-300">
       {children}
-      <div className="absolute -bottom-12 left-0 block w-full space-x-4 text-center text-xs font-semibold md:text-sm ">
-        <span className="select-none text-zinc-500">계정이 없으신가요?</span>
-        <Link
-          href="/signup"
-          className="cursor-pointer font-semibold text-camp-heavy"
-        >
-          가입하기
-        </Link>
+      <div className="absolute -bottom-16 left-0 flex w-full flex-col justify-center space-y-2 text-center text-xs font-semibold md:text-sm ">
+        <div className="space-x-4">
+          <span className="select-none text-zinc-500">계정이 없으신가요?</span>
+          <Link
+            href="/signup"
+            className="cursor-pointer font-semibold text-camp-heavy"
+          >
+            가입하기
+          </Link>
+        </div>
+        <div className="flex select-none flex-col text-zinc-700 dark:text-zinc-500">
+          <span>테스트용 계정</span>
+          <span>host001 / host@123</span>
+        </div>
       </div>
     </div>
   );
--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { Loader2 } from 'lucide-react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 import Input from '@/components/common/input';
 import { useModalStore } from '@/store/use-modal-store';
@@ -46,31 +47,35 @@ export default function LoginPage() {
     });
   };
 
+  const [onDialog, setOnDialog] = useState(true);
+
   return (
-    <form
-      onSubmit={handleSubmit(onValid)}
-      className="flex flex-1 flex-col space-y-12"
-    >
-      <Input
-        label="아이디"
-        placeholder="아이디를 입력해 주세요."
-        type="text"
-        warning={errors.userId?.message}
-        register={register('userId')}
-      />
-      <Input
-        label="비밀번호"
-        placeholder="비밀번호를 입력해 주세요."
-        type="password"
-        warning={errors.userPw?.message}
-        register={register('userPw')}
-      />
-      <button
-        disabled={isSubmitting}
-        className="flex w-full justify-center rounded-sm bg-camp-heavy py-2 text-center font-semibold text-white dark:bg-dark-400"
+    <>
+      <form
+        onSubmit={handleSubmit(onValid)}
+        className="flex flex-1 flex-col space-y-12"
       >
-        {isSubmitting ? <Loader2 className="animate-spin" /> : '로그인'}
-      </button>
-    </form>
+        <Input
+          label="아이디"
+          placeholder="아이디를 입력해 주세요."
+          type="text"
+          warning={errors.userId?.message}
+          register={register('userId')}
+        />
+        <Input
+          label="비밀번호"
+          placeholder="비밀번호를 입력해 주세요."
+          type="password"
+          warning={errors.userPw?.message}
+          register={register('userPw')}
+        />
+        <button
+          disabled={isSubmitting}
+          className="flex w-full justify-center rounded-sm bg-camp-heavy py-2 text-center font-semibold text-white dark:bg-dark-400"
+        >
+          {isSubmitting ? <Loader2 className="animate-spin" /> : '로그인'}
+        </button>
+      </form>
+    </>
   );
 }
