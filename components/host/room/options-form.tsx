1ff7c5001f73488f95f54ccb9847ff6e130044e7
kjw-blog <rkdwjddnr11@naver.com>
2023-12-15 20:49:10 +0900

    feat: 객실 화면
    - 객실 옵션 form 분리
    - input number 화살표 삭제

---
+++ components/host/room/options-form.tsx
@@ -0,0 +1,100 @@
+import { useRoomStore } from '@/store/use-room-data';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Minus, Plus } from 'lucide-react';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const Form = z.object({
+  baseGuestNumber: z.number(),
+  maximumGuestNumber: z.number(),
+  additionalPrice: z.number(),
+  peakSeasonPrice: z.number(),
+  semiPeakSeasonPrice: z.number(),
+  offSeasonPrice: z.number(),
+});
+
+interface OptionsFormProps {
+  onSubmitting: (value: boolean) => void;
+}
+
+export const OptionsForm = ({ onSubmitting }: OptionsFormProps) => {
+  const { room } = useRoomStore();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<z.infer<typeof Form>>({
+    resolver: zodResolver(Form),
+    defaultValues: {
+      additionalPrice: 0,
+      baseGuestNumber: 0,
+      maximumGuestNumber: 0,
+      offSeasonPrice: 0,
+      peakSeasonPrice: 0,
+      semiPeakSeasonPrice: 0,
+    },
+  });
+
+  const onValid = (data: z.infer<typeof Form>) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    onSubmitting(isSubmitting);
+  }, [isSubmitting, onSubmitting]);
+
+  return (
+    <form
+      id="save-room-options"
+      onSubmit={handleSubmit(onValid)}
+      className="space-y-3"
+    >
+      <div className="grid w-full auto-rows-[minmax(30px,auto)] grid-cols-10 items-center space-x-3">
+        <span className="col-[1/3]">기준 인원</span>
+        <div className="col-[3/11] flex h-full">
+          <button
+            type="button"
+            className="rounded-bl-md rounded-tl-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600"
+          >
+            <Minus className="h-4 w-4" />
+          </button>
+          <input
+            type="number"
+            className="flex-1 border-y-2 px-2 outline-none dark:border-none"
+            {...register('baseGuestNumber')}
+          />
+          <button
+            type="button"
+            className="rounded-br-md rounded-tr-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600"
+          >
+            <Plus className="h-4 w-4" />
+          </button>
+        </div>
+      </div>
+      <div className="grid w-full auto-rows-[minmax(30px,auto)] grid-cols-10 items-center space-x-3">
+        <span className="col-[1/3]">최대 인원</span>
+        <div className="col-[3/11] flex h-full">
+          <button
+            type="button"
+            className="rounded-bl-md rounded-tl-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600"
+          >
+            <Minus className="h-4 w-4" />
+          </button>
+          <input
+            type="number"
+            className="flex-1 border-y-2 px-2 outline-none dark:border-none"
+            {...register('maximumGuestNumber')}
+          />
+          <button
+            type="button"
+            className="rounded-br-md rounded-tr-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600"
+          >
+            <Plus className="h-4 w-4" />
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
--- components/modal/room/options-modal.tsx
+++ components/modal/room/options-modal.tsx
@@ -1,47 +1,26 @@
-import { z } from 'zod';
+import { Loader2 } from 'lucide-react';
 
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog';
 import { useModalStore } from '@/store/use-modal-store';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useRoomStore } from '@/store/use-room-data';
-import { Loader2, Minus, Plus } from 'lucide-react';
 
-const Form = z.object({
-  baseGuestNumber: z.number(),
-  maximumGuestNumber: z.number(),
-  additionalPrice: z.number(),
-  peakSeasonPrice: z.number(),
-  semiPeakSeasonPrice: z.number(),
-  offSeasonPrice: z.number(),
-});
+import { OptionsForm } from '@/components/host/room/options-form';
+import { useCallback, useState } from 'react';
 
 export const OptionsModal = () => {
   const { isOpen, type, closeModal } = useModalStore();
-  const { room } = useRoomStore();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const open = isOpen && type === 'room-options';
 
-  const {
-    formState: { isSubmitting },
-  } = useForm<z.infer<typeof Form>>({
-    resolver: zodResolver(Form),
-    defaultValues: {
-      additionalPrice: 0,
-      baseGuestNumber: 0,
-      maximumGuestNumber: 0,
-      offSeasonPrice: 0,
-      peakSeasonPrice: 0,
-      semiPeakSeasonPrice: 0,
-    },
-  });
+  const onSubmitting = useCallback((value: boolean) => {
+    setIsSubmitting(value);
+  }, []);
 
   return (
     <Dialog open={open} onOpenChange={closeModal}>
@@ -50,42 +29,13 @@ export const OptionsModal = () => {
           <DialogTitle className="bg-camp-heavy py-3 pl-4 text-left text-sm font-bold text-white">
             객실 설정
           </DialogTitle>
-          <DialogDescription className="select-none space-y-3 p-3 text-xs font-bold text-zinc-600 dark:text-zinc-400">
-            <div className="grid w-full auto-rows-[minmax(30px,auto)] grid-cols-10 items-center space-x-3">
-              <span className="col-[1/3]">기준 인원</span>
-              <div className="col-[3/11] flex h-full">
-                <button className="rounded-bl-md rounded-tl-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600">
-                  <Minus className="h-4 w-4" />
-                </button>
-                <input
-                  type="text"
-                  className="flex-1 border-y-2 px-2 outline-none dark:border-none"
-                />
-                <button className="rounded-br-md rounded-tr-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600">
-                  <Plus className="h-4 w-4" />
-                </button>
-              </div>
-            </div>
-            <div className="grid w-full auto-rows-[minmax(30px,auto)] grid-cols-10 items-center space-x-3">
-              <span className="col-[1/3]">최대 인원</span>
-              <div className="col-[3/11] flex h-full">
-                <button className="rounded-bl-md rounded-tl-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600">
-                  <Minus className="h-4 w-4" />
-                </button>
-                <input
-                  type="text"
-                  className="flex-1 border-y-2 px-2 outline-none dark:border-none"
-                />
-                <button className="rounded-br-md rounded-tr-md border-2 border-zinc-200 px-1 dark:border-none dark:bg-zinc-600">
-                  <Plus className="h-4 w-4" />
-                </button>
-              </div>
-            </div>
+          <DialogDescription className="select-none p-3 text-xs font-bold text-zinc-600 dark:text-zinc-400">
+            <OptionsForm onSubmitting={onSubmitting} />
           </DialogDescription>
         </DialogHeader>
         <div className="ml-auto flex space-x-2 px-3 py-2">
           <button
-            onClick={closeModal}
+            form="save-room-options"
             className="h-[30px] w-[50px] rounded-md bg-camp-heavy text-sm font-bold text-white transition hover:bg-camp-heavy/70"
           >
             {isSubmitting ? (
@@ -96,6 +46,7 @@ export const OptionsModal = () => {
           </button>
           <button
             onClick={closeModal}
+            type="button"
             className="h-[30px] w-[50px] rounded-md bg-camp-heavy  text-sm font-bold text-white transition hover:bg-camp-heavy/70"
           >
             닫기
