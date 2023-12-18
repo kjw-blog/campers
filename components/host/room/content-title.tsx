96b5075681745e21ecc6a08f205413862f69f4ee
kjw-blog <rkdwjddnr11@naver.com>
2023-12-18 20:09:02 +0900

    feat: 차트 개발
    - 년도 선택 개발중

--- components/host/room/content-title.tsx
+++ components/host/room/content-title.tsx
@@ -5,10 +5,8 @@ interface ContentTitleProps {
 
 export const ContentTitle = ({ title, children }: ContentTitleProps) => {
   return (
-    <header className="flex h-10 w-full items-center justify-between bg-zinc-300 px-2 dark:bg-zinc-800">
-      <span className="select-none font-bold text-zinc-700 dark:text-zinc-400">
-        {title}
-      </span>
+    <header className="flex h-10 w-full select-none items-center justify-between bg-zinc-300 px-2 font-bold text-zinc-700 dark:bg-zinc-800 dark:text-zinc-400">
+      <span>{title}</span>
       {children}
     </header>
   );
--- components/host/room/graph-content.tsx
+++ components/host/room/graph-content.tsx
@@ -1,10 +1,35 @@
+import { PlayIcon } from 'lucide-react';
+import { useState } from 'react';
+
 import { ContentTitle } from './content-title';
 import { Graph } from './graph';
 
+const thisYear = new Date().getFullYear();
+
 export const GraphContent = () => {
+  const [selectedYear, setSelectedYear] = useState<number>(2023);
+
+  const onClick = (type: 'prev' | 'next') => {
+    setSelectedYear((prevYear) => prevYear + (type === 'prev' ? -1 : 1));
+  };
+
   return (
     <div className="flex flex-col grid-in-graph">
-      <ContentTitle title="매출 그래프" />
+      <ContentTitle title="매출 그래프">
+        <div className="flex items-center space-x-1">
+          <button onClick={onClick.bind(null, 'prev')}>
+            <PlayIcon className="h-3 w-3 rotate-180 fill-zinc-700 dark:fill-zinc-400" />
+          </button>
+          <button className="text-sm">{selectedYear}</button>
+          <button
+            onClick={onClick.bind(null, 'next')}
+            className="group"
+            disabled={selectedYear >= thisYear}
+          >
+            <PlayIcon className="h-3 w-3 fill-zinc-700 group-disabled:invisible  dark:fill-zinc-400" />
+          </button>
+        </div>
+      </ContentTitle>
       <Graph />
     </div>
   );
