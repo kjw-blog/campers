9dd1dffbc7fe4f235f9399bd87bde636954e7383
kjw-blog <rkdwjddnr11@naver.com>
2023-12-18 20:59:05 +0900

    feat: 차트 개발
    - 년도 선택 달력 완성

--- components/common/year-calendar.tsx
+++ components/common/year-calendar.tsx
@@ -7,10 +7,24 @@ const thisYear = new Date().getFullYear();
 export const YearCalendar = () => {
   const calendarRef = useRef<HTMLDivElement>(null);
   const [calendarOpen, setCalendarOpen] = useState(false);
-  const [selectedYear, setSelectedYear] = useState<number>(2023);
+  const [selectedYear, setSelectedYear] = useState<number>(thisYear);
+  const [baseYear, setBaseYear] = useState<number>(thisYear);
 
   const onClick = (type: 'prev' | 'next') => {
     setSelectedYear((prevYear) => prevYear + (type === 'prev' ? -1 : 1));
+    setBaseYear((prevYear) => prevYear + (type === 'prev' ? -1 : 1));
+  };
+
+  const onBasePrevClick = () => {
+    setBaseYear((prev) => prev - 10);
+  };
+
+  const onBaseNextClick = () => {
+    if (baseYear + 10 < thisYear) {
+      setBaseYear((prev) => prev + 10);
+    } else {
+      setBaseYear(thisYear);
+    }
   };
 
   useEffect(() => {
@@ -28,11 +42,14 @@ export const YearCalendar = () => {
   }, []);
 
   return (
-    <div className="relative flex items-center space-x-2">
+    <div ref={calendarRef} className="relative flex items-center space-x-2">
       <button onClick={onClick.bind(null, 'prev')}>
         <PlayIcon className="h-3 w-3 rotate-180 fill-zinc-700 dark:fill-zinc-400" />
       </button>
-      <button className="text-sm" onClick={() => setCalendarOpen(true)}>
+      <button
+        className="text-sm"
+        onClick={() => setCalendarOpen((prev) => !prev)}
+      >
         {selectedYear}
       </button>
       <button
@@ -48,16 +65,21 @@ export const YearCalendar = () => {
             initial={{ opacity: 0, translateY: 10 }}
             animate={{ opacity: 1, translateY: 0 }}
             exit={{ opacity: 0, translateY: 10 }}
-            ref={calendarRef}
             className="absolute right-0 top-6 z-[99] rounded-md border-[1px] border-zinc-400 bg-white p-2 text-center text-sm text-zinc-600 dark:bg-dark-300 dark:text-zinc-400"
           >
             <div className="flex w-full items-center justify-between rounded-md border-b-2 border-b-zinc-400 px-3 py-2">
-              <button>
+              <button onClick={onBasePrevClick}>
                 <PlayIcon className="h-3 w-3 rotate-180 fill-zinc-700 dark:fill-zinc-400" />
               </button>
-              <span>2014 ~ 2023</span>
-              <button>
-                <PlayIcon className="h-3 w-3 fill-zinc-700 dark:fill-zinc-400" />
+              <span>
+                {baseYear - 9} ~ {baseYear}
+              </span>
+              <button
+                onClick={onBaseNextClick}
+                className="group"
+                disabled={baseYear >= thisYear}
+              >
+                <PlayIcon className="h-3 w-3 fill-zinc-700 group-disabled:invisible dark:fill-zinc-400" />
               </button>
             </div>
             <div className="grid auto-rows-[minmax(40px,auto)] grid-cols-[repeat(2,minmax(120px,auto))] gap-1 [&>*]:rounded-md [&>*]:duration-300">
@@ -65,13 +87,14 @@ export const YearCalendar = () => {
                 .map((_, i) => (
                   <button
                     onClick={() => {
-                      setSelectedYear(selectedYear - i);
+                      setSelectedYear(baseYear - i);
+                      setBaseYear(baseYear - i);
                       setCalendarOpen(false);
                     }}
                     className=" hover:bg-zinc-200 dark:hover:bg-zinc-700"
                     key={i}
                   >
-                    {selectedYear - i}
+                    {baseYear - i}
                   </button>
                 ))
                 .reverse()}
