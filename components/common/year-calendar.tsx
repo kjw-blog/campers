c517ab52fcc6817cd6b9d0eb6cb8f6d8af15b999
kjw-blog <rkdwjddnr11@naver.com>
2023-12-18 20:47:47 +0900

    feat: 차트 개발
    - 달력 framer-motion 적용
    - 달력 컴포넌트 분리

---
+++ components/common/year-calendar.tsx
@@ -0,0 +1,84 @@
+import { PlayIcon } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const thisYear = new Date().getFullYear();
+
+export const YearCalendar = () => {
+  const calendarRef = useRef<HTMLDivElement>(null);
+  const [calendarOpen, setCalendarOpen] = useState(false);
+  const [selectedYear, setSelectedYear] = useState<number>(2023);
+
+  const onClick = (type: 'prev' | 'next') => {
+    setSelectedYear((prevYear) => prevYear + (type === 'prev' ? -1 : 1));
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: Event) => {
+      if (!calendarRef.current?.contains(e.target as Node)) {
+        setCalendarOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="relative flex items-center space-x-2">
+      <button onClick={onClick.bind(null, 'prev')}>
+        <PlayIcon className="h-3 w-3 rotate-180 fill-zinc-700 dark:fill-zinc-400" />
+      </button>
+      <button className="text-sm" onClick={() => setCalendarOpen(true)}>
+        {selectedYear}
+      </button>
+      <button
+        onClick={onClick.bind(null, 'next')}
+        className="group"
+        disabled={selectedYear >= thisYear}
+      >
+        <PlayIcon className="h-3 w-3 fill-zinc-700 group-disabled:invisible  dark:fill-zinc-400" />
+      </button>
+      <AnimatePresence>
+        {calendarOpen && (
+          <motion.div
+            initial={{ opacity: 0, translateY: 10 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0, translateY: 10 }}
+            ref={calendarRef}
+            className="absolute right-0 top-6 z-[99] rounded-md border-[1px] border-zinc-400 bg-white p-2 text-center text-sm text-zinc-600 dark:bg-dark-300 dark:text-zinc-400"
+          >
+            <div className="flex w-full items-center justify-between rounded-md border-b-2 border-b-zinc-400 px-3 py-2">
+              <button>
+                <PlayIcon className="h-3 w-3 rotate-180 fill-zinc-700 dark:fill-zinc-400" />
+              </button>
+              <span>2014 ~ 2023</span>
+              <button>
+                <PlayIcon className="h-3 w-3 fill-zinc-700 dark:fill-zinc-400" />
+              </button>
+            </div>
+            <div className="grid auto-rows-[minmax(40px,auto)] grid-cols-[repeat(2,minmax(120px,auto))] gap-1 [&>*]:rounded-md [&>*]:duration-300">
+              {Array.from({ length: 10 })
+                .map((_, i) => (
+                  <button
+                    onClick={() => {
+                      setSelectedYear(selectedYear - i);
+                      setCalendarOpen(false);
+                    }}
+                    className=" hover:bg-zinc-200 dark:hover:bg-zinc-700"
+                    key={i}
+                  >
+                    {selectedYear - i}
+                  </button>
+                ))
+                .reverse()}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
