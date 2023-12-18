a3678387761ab667d40a677c40145a8e47427363
kjw-blog <rkdwjddnr11@naver.com>
2023-12-18 20:34:31 +0900

    feat: 차트 개발
    - 년도 달력 개발중

--- components/host/room/graph-content.tsx
+++ components/host/room/graph-content.tsx
@@ -7,6 +7,7 @@ import { Graph } from './graph';
 const thisYear = new Date().getFullYear();
 
 export const GraphContent = () => {
+  const [calendarOpen, setCalendarOpen] = useState(false);
   const [selectedYear, setSelectedYear] = useState<number>(2023);
 
   const onClick = (type: 'prev' | 'next') => {
@@ -16,11 +17,13 @@ export const GraphContent = () => {
   return (
     <div className="flex flex-col grid-in-graph">
       <ContentTitle title="매출 그래프">
-        <div className="flex items-center space-x-1">
+        <div className="relative flex items-center space-x-1">
           <button onClick={onClick.bind(null, 'prev')}>
             <PlayIcon className="h-3 w-3 rotate-180 fill-zinc-700 dark:fill-zinc-400" />
           </button>
-          <button className="text-sm">{selectedYear}</button>
+          <button className="text-sm" onClick={() => setCalendarOpen(true)}>
+            {selectedYear}
+          </button>
           <button
             onClick={onClick.bind(null, 'next')}
             className="group"
@@ -28,6 +31,26 @@ export const GraphContent = () => {
           >
             <PlayIcon className="h-3 w-3 fill-zinc-700 group-disabled:invisible  dark:fill-zinc-400" />
           </button>
+          {calendarOpen && (
+            <div className="absolute right-0 top-6 z-[99] rounded-md border-[1px] border-zinc-400 bg-white p-2 text-center text-sm text-zinc-600 dark:bg-dark-300 dark:text-zinc-400">
+              <div className="grid auto-rows-[minmax(40px,auto)] grid-cols-[repeat(2,minmax(120px,auto))] gap-1 [&>*]:rounded-md [&>*]:duration-300">
+                {Array.from({ length: 10 })
+                  .map((_, i) => (
+                    <button
+                      onClick={() => {
+                        setSelectedYear(selectedYear - i);
+                        setCalendarOpen(false);
+                      }}
+                      className=" hover:bg-zinc-200 dark:hover:bg-zinc-700"
+                      key={i}
+                    >
+                      {selectedYear - i}
+                    </button>
+                  ))
+                  .reverse()}
+              </div>
+            </div>
+          )}
         </div>
       </ContentTitle>
       <Graph />
