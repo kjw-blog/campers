3923c73cc7c0f347ee67f304b9162c26b2f8a498
kjw-blog <rkdwjddnr11@naver.com>
2023-12-19 20:22:41 +0900

    feat: 객실 화면
    - 더미 이미지 추가
    - swiper 추가 및 적용
    - swiper 버튼 css 수정

--- components/host/room/picture.tsx
+++ components/host/room/picture.tsx
@@ -1,11 +1,58 @@
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+import { MoreVertical } from 'lucide-react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Image from 'next/image';
+
 import { ContentTitle } from './content-title';
+import { useRoomStore } from '@/store/use-room-data';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export const Picture = () => {
+  const { room } = useRoomStore();
+
+  const onClick = () => {
+    alert('click');
+  };
+
   return (
-    <div className="flex flex-col grid-in-picture">
-      <ContentTitle title="객실 사진" />
+    <div className="flex select-none flex-col grid-in-picture">
+      <ContentTitle title="객실 사진">
+        <button onClick={onClick}>
+          <MoreVertical className="h-4 w-4" />
+        </button>
+      </ContentTitle>
       <div className="flex flex-1 items-center justify-center text-xs font-bold text-zinc-700 dark:text-zinc-400">
-        객실 사진이 없습니다.
+        {/* {room?.roomImage.length === 0 && '객실 사진이 없습니다.'} */}
+        <div className="relative h-full w-full">
+          <Swiper
+            modules={[Navigation, Pagination, A11y, Autoplay]}
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            navigation
+            autoplay={{ delay: 3000 }}
+            loop={true}
+            rewind={true}
+            className="h-full w-full"
+          >
+            {Array.from({ length: 5 }).map((_, i) => (
+              <SwiperSlide key={i} className="relative h-full w-full">
+                <Image
+                  src="https://utfs.io/f/e3479ef9-00ee-4e28-9a35-2da302204baa-3jsln.JPG"
+                  alt="객실사진"
+                  fill
+                  style={{
+                    objectFit: 'contain',
+                  }}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
