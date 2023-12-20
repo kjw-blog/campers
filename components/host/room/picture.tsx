5296620e9c5c5417f08a73553e9095dc30b49479
kjw-blog <rkdwjddnr11@naver.com>
2023-12-20 17:09:52 +0900

    feat: 객실 화면
    - 이미지 캡션 추가

--- components/host/room/picture.tsx
+++ components/host/room/picture.tsx
@@ -29,26 +29,32 @@ export const Picture = () => {
         {/* {room?.roomImage.length === 0 && '객실 사진이 없습니다.'} */}
         <div className="relative h-full w-full">
           <Swiper
-            modules={[Navigation, Pagination, A11y, Autoplay]}
+            modules={[Navigation, A11y, Autoplay]}
             spaceBetween={50}
             slidesPerView={1}
-            pagination={{ clickable: true }}
             navigation
-            autoplay={{ delay: 3000, pauseOnMouseEnter: true }}
+            autoplay={{ delay: 5000, pauseOnMouseEnter: true }}
             loop={true}
             rewind={true}
             className="h-full w-full"
           >
             {Array.from({ length: 5 }).map((_, i) => (
-              <SwiperSlide key={i} className="relative h-full w-full">
-                <Image
-                  src="https://utfs.io/f/e3479ef9-00ee-4e28-9a35-2da302204baa-3jsln.JPG"
-                  alt="객실사진"
-                  fill
-                  style={{
-                    objectFit: 'contain',
-                  }}
-                />
+              <SwiperSlide key={i} className="h-full w-full">
+                <div className="relative h-full w-full">
+                  <Image
+                    src="https://utfs.io/f/e3479ef9-00ee-4e28-9a35-2da302204baa-3jsln.JPG"
+                    alt="객실사진"
+                    fill
+                    style={{
+                      objectFit: 'contain',
+                    }}
+                  />
+                  <div className="absolute bottom-2 right-0 flex items-center space-x-2 rounded-bl-md rounded-tl-md bg-zinc-300/50 px-2 py-1 text-[10px] text-zinc-700 dark:bg-zinc-700/70 dark:text-zinc-400 md:max-w-[100px]">
+                    <span>일본</span>
+                    <div className="h-[10px] w-[2px] bg-zinc-400 dark:bg-zinc-600" />
+                    <div className="">{i + 1}/5</div>
+                  </div>
+                </div>
               </SwiperSlide>
             ))}
           </Swiper>
