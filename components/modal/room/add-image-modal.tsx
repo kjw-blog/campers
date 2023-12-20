f37cc8a622207f92fd3bf5c5abab78d407c47645
kjw-blog <rkdwjddnr11@naver.com>
2023-12-20 17:53:19 +0900

    feat: 객실 화면
    - 이미지 모달 창 임시 추가
    - 스크롤바 전역 css 추가

--- components/modal/room/add-image-modal.tsx
+++ components/modal/room/add-image-modal.tsx
@@ -1,16 +1,9 @@
 import { Loader2 } from 'lucide-react';
-
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
-import { useModalStore } from '@/store/use-modal-store';
-
-import { OptionsForm } from '@/components/host/room/options-form';
 import { useCallback, useState } from 'react';
+
+import { useModalStore } from '@/store/use-modal-store';
+import { OptionsForm } from '@/components/host/room/options-form';
+import { Modal } from './modal';
 
 export const AddImageModal = () => {
   const { isOpen, type, closeModal } = useModalStore();
@@ -23,17 +16,21 @@ export const AddImageModal = () => {
   }, []);
 
   return (
-    <Dialog open={open} onOpenChange={closeModal}>
-      <DialogContent className="z-50 w-[450px] max-w-[95%] gap-0 overflow-hidden rounded-md p-0 dark:bg-dark-200">
-        <DialogHeader>
-          <DialogTitle className="bg-camp-heavy py-3 pl-4 text-left text-sm font-bold text-white">
-            객실 설정
-          </DialogTitle>
-          <div className="select-none p-3 text-xs font-bold text-zinc-600 dark:text-zinc-400">
-            <OptionsForm onSubmitting={onSubmitting} onClose={closeModal} />
+    <Modal open={open} onClose={closeModal}>
+      <div className="z-50 flex w-[500px] max-w-[100%] flex-col gap-0 space-y-2 overflow-hidden rounded-md p-0 dark:bg-dark-200">
+        <header>
+          <div className="bg-camp-heavy py-3 pl-4 text-left text-sm font-bold text-white">
+            객실 사진
           </div>
-        </DialogHeader>
-        <div className="ml-auto flex space-x-2 px-3 py-2">
+        </header>
+        <div className="max-h-[400px] w-full select-none divide-y-2 divide-zinc-700 overflow-y-auto p-3  text-xs font-bold text-zinc-600 dark:divide-zinc-400 dark:text-zinc-400">
+          {Array.from({ length: 10 }).map((_, i) => (
+            <div key={i} className="flex h-[80px] w-full items-center py-2">
+              이미지 {i + 1}
+            </div>
+          ))}
+        </div>
+        <div className="ml-auto flex justify-end space-x-2 px-3 py-2">
           <button
             form="save-room-options"
             className="h-[30px] w-[50px] rounded-md bg-camp-heavy text-sm font-bold text-white transition hover:bg-camp-heavy/70"
@@ -52,7 +49,7 @@ export const AddImageModal = () => {
             닫기
           </button>
         </div>
-      </DialogContent>
-    </Dialog>
+      </div>
+    </Modal>
   );
 };
---
+++ components/modal/room/modal.tsx
@@ -0,0 +1,46 @@
+import { motion } from 'framer-motion';
+import { createPortal } from 'react-dom';
+import { useEffect, useState } from 'react';
+
+import { BackDrop } from '@/components/common/backdrop';
+
+interface ModalProps {
+  onClose: () => void;
+  open: boolean;
+  children: React.ReactNode;
+}
+
+export const Modal = ({ onClose, children, open }: ModalProps) => {
+  const [portalElement, setPortalElement] = useState<HTMLElement | null>(null);
+  const [backdropElement, setBackdropElement] = useState<HTMLElement | null>(
+    null,
+  );
+
+  useEffect(() => {
+    const portal = document.getElementById('modal-portal');
+    const backdrop = document.getElementById('backdrop');
+
+    setPortalElement(portal);
+    setBackdropElement(backdrop);
+  }, []);
+
+  if (!portalElement || !backdropElement || !open) return;
+
+  return (
+    <>
+      {createPortal(<BackDrop onClose={onClose} />, backdropElement)}
+      {createPortal(
+        <motion.dialog
+          open
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="fixed left-1/2 top-1/2 z-[100] mx-0 max-w-[95vw] -translate-x-[50%] -translate-y-[50%] overflow-hidden rounded-md border border-zinc-300 p-0 shadow-2xl dark:border-zinc-500 dark:bg-dark-200 dark:shadow-dark_md"
+        >
+          {children}
+        </motion.dialog>,
+        portalElement,
+      )}
+    </>
+  );
+};
